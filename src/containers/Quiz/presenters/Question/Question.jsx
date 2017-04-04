4e5aa337c3b68a94ee60f87e34f4a85a4c7abde7
Andrew Wong <arwong09@gmail.com>
2017-04-03 19:13:24 -0500

    add transitions to questions

--- src/containers/Quiz/presenters/Question/Question.jsx
+++ src/containers/Quiz/presenters/Question/Question.jsx
@@ -2,6 +2,7 @@ import style from './question.scss'
 import React, { Component } from 'react'
 import store from 'redux/store'
 import { submitAnswer } from 'redux/actions'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import QuestionChoice from './presenters/QuestionChoice/QuestionChoice.jsx'
 
 export default class Question extends Component {
@@ -10,6 +11,10 @@ export default class Question extends Component {
     this.handleSelect = this.handleSelect.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.state = { selected: null };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true
   }
 
   handleSelect(value) {
@@ -26,42 +31,49 @@ export default class Question extends Component {
 
   render() {
     return(
-      <div className="question">
-        <div>{this.props.data.text}</div>
-        {this.state.selected ? (
-          <div className="question__choices">
-            {Object.keys(this.props.data.choices).map((choice) => (
-              <QuestionChoice
-                className={this.state.selected == choice ? (
-                  "question__choice--selected"
-                ) : (
-                  "question__choice--unselected disabled"
-                )}
-                text={this.props.data.choices[choice]}
-                value={choice}
-                onClick={this.handleSelect}
-                key={this.props.data.id + '-' + choice} />
-            ))}
-            <div className="button--submit" onClick={this.handleSubmit}>
-              Submit
+      <ReactCSSTransitionGroup
+        transitionName="question"
+        transitionAppear={true}
+        transitionAppearTimeout={400}
+        transitionEnterTimeout={400}
+        transitionLeave={false}>
+        <div className="question" key={this.props.data.id}>
+          <div>{this.props.data.text}</div>
+          {this.state.selected ? (
+            <div className="question__choices">
+              {Object.keys(this.props.data.choices).map((choice) => (
+                <QuestionChoice
+                  className={this.state.selected == choice ? (
+                    "question__choice--selected"
+                  ) : (
+                    "question__choice--unselected disabled"
+                  )}
+                  text={this.props.data.choices[choice]}
+                  value={choice}
+                  onClick={this.handleSelect}
+                  key={this.props.data.id + '-' + choice} />
+              ))}
+              <div className="button--submit" onClick={this.handleSubmit}>
+                Submit
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="question__choices">
-            {Object.keys(this.props.data.choices).map((choice) => (
-              <QuestionChoice
-                className="question__choice--unselected"
-                text={this.props.data.choices[choice]}
-                value={choice}
-                onClick={this.handleSelect}
-                key={this.props.data.id + '-' + choice} />
-            ))}
-            <div className="button--submit disabled">
-              Submit
+          ) : (
+            <div className="question__choices">
+              {Object.keys(this.props.data.choices).map((choice) => (
+                <QuestionChoice
+                  className="question__choice--unselected"
+                  text={this.props.data.choices[choice]}
+                  value={choice}
+                  onClick={this.handleSelect}
+                  key={this.props.data.id + '-' + choice} />
+              ))}
+              <div className="button--submit disabled">
+                Submit
+              </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </ReactCSSTransitionGroup>
     )
   }
 }
